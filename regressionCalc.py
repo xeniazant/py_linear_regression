1a0058aaffad700aeb20e6b142f724fbc201fae0
xeniazant <xeniazant@gmail.com>
2019-10-13 10:30:01 -0400

    fleshed out structure of program. Added foundation for UI.

--- regressionCalc.py
+++ regressionCalc.py
@@ -2,5 +2,28 @@
 # Linear Regression calc by Xenia Zantello
 #
 
+import tkinter
+
+# take user input? and put in list or tuples?
+def points_in():
+    return
+
+# index through list and construct function to minimize
+def make_function():
+    return
+
+# Algorithm that minimizes the function. Binary search?
+def find_fit():
+    return
+
+
+
+#--------main code below, calling previous functions to determine best fit--------
+print("Hello world")
+
+# tkinter ui window
+
+m = tkinter.Tk()
+m.mainloop()
 
 
