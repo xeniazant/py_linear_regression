f56b69844962fd894eabe84c6db83e360c2793d3
xeniazant <xeniazant@gmail.com>
2019-10-14 09:04:59 -0400

    figured out basic input functionality to take user's list of points and convert them to a set of tuples that I can do math on

--- regressionCalc.py
+++ regressionCalc.py
@@ -5,6 +5,9 @@
 import tkinter
 
 # take user input? and put in list or tuples?
+from typing import List
+
+
 def points_in():
     return
 
@@ -19,7 +22,20 @@ def find_fit():
 
 
 #--------main code below, calling previous functions to determine best fit--------
-print("Hello world")
+print("Please enter a set of ordered pairs.")
+points = input()
+new_points = points.replace(",", "")
+points_list = new_points.split(" ")
+for p in points_list:
+    p = int(p)
+print(points_list)
+points_set = zip(*points_list)
+
+print(set(points_set))
+
+
+
+print("Values entered were: ", points)
 
 # tkinter ui window
 
